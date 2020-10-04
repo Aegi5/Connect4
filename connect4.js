3a8d6cd20ff235bc56265255bada565a7129bdf5
Aegis <hyunchulchoi98@gmail.com>
2020-10-04 18:32:52 +0200

    final change allowing alternate player to check if someone has won

--- connect4.js
+++ connect4.js
@@ -3,15 +3,20 @@ class Connect4{
     this.ROWS = 6;
     this.COLS = 7;
     this.selector = selector;
+    this.isGameOver = false;
+    this.onPlayerMove = function(){} //call back function
     this.createGrid();
     this.player = 'red';
     this.setupEventListener();
+
   }
 //NB : his convention, prefix jQuery-obtained DOM obj by a $ chara
 
   createGrid(){
     const $board = $(this.selector);
-    console.log($board);
+    $board.empty();
+    this.isGameOver = false;
+    this.player = 'red';
     for(let row = 0; row < this.ROWS; row++){
       const $row = $('<div>')
         .addClass('row');
@@ -24,7 +29,6 @@ class Connect4{
       }
       $board.append($row);
     }
-    console.log($board.html());
   }
 
   setupEventListener() {
@@ -33,7 +37,6 @@ class Connect4{
 
      function findLastEmptyCell(col){
        const cells = $(`.col[data-col = ${col}]`);
-       console.log(cells);
        for(let i = cells.length -1;i >=0; i--){
          const $cell = $(cells[i]);
          if($cell.hasClass('empty')){
@@ -44,7 +47,6 @@ class Connect4{
      }
 
      $board.on('mouseenter', '.col.empty', function(){
-       console.log(this);
        const col = $(this).data('col');
        const $lastEmptyCell = findLastEmptyCell(col);
        $lastEmptyCell.addClass(`next-${that.player}`);
@@ -55,14 +57,95 @@ class Connect4{
      });
 
      $board.on('click', '.col.empty', function(){
+       if(that.isGameOver) return;
        const col = $(this).data('col');
        const $lastEmptyCell = findLastEmptyCell(col);
+
        $lastEmptyCell.removeClass(`empty next-${that.player}` );
        $lastEmptyCell.addClass(that.player);
        //this is the event called 'this', rn we need the original this which point to the DOM element connect4 ? so added
        //a original field
+       $lastEmptyCell.data('player', that.player);
+
+       const winner = that.checkForWinner(
+         $lastEmptyCell.data('row'),
+         $lastEmptyCell.data('col'));
+       if(winner){
+         //do stuff
+         alert(`Game overt ! Player ${that.player} has won`);
+         that.isGameOver = true;
+         $('.col.empty').removeClass('empty');
+         return;
+       }
+
        that.player = (that.player == 'red') ? 'blue': 'red';
+       that.onPlayerMove();
        $(this).trigger('mouseenter');
+
      });
   }
+
+  //row cow last placed
+  checkForWinner(row, col){
+    const that = this;
+
+    function $getCell(i, j){
+      return $(`.col[data-row='${i}'][data-col='${j}']`);
+    }
+
+    function checkDirection(direction){
+      let total = 0;
+      let i = row + direction.i;
+      let j = col + direction.j;
+      let $next = $getCell(i,j);
+      while (i >= 0 &&
+      i < that.ROWS &&
+      j >= 0 &&
+      j < that.COLS &&
+      $next.data('player') === that.player){
+        total++;
+        i += direction.i;
+        j += direction.j;
+        $next = $getCell(i,j);
+      }
+      return total;
+    }
+
+    function checkWin(directionA, directionB){
+      const total = 1 +
+        checkDirection(directionA) +
+        checkDirection(directionB);
+      if(total >= 4){
+        return that.player;
+      }
+      else {
+        return null;
+      }
+    }
+
+    function checkVerticals(){
+      return checkWin({i: -1, j: 0}, {i: 1, j: 0});//direction
+    }
+
+    function checkHorizontals(){
+      return checkWin({i: 0, j: -1}, {i: 0, j: 1});
+    }
+
+    function checkDiagonalBLtoTR(){
+      return checkWin({i:-1, j: -1}, {i:1, j: 1});
+    }
+    function checkDiagonalBRtoTL(){
+      return checkWin({i:1, j: -1}, {i:-1, j: 1});
+    }
+
+    return checkVerticals() || checkHorizontals() || checkDiagonalBLtoTR() || checkDiagonalBRtoTL();
+  }
+
+  restart(){
+    this.createGrid();
+    this.onPlayerMove();
+  }
+
+
+
 }
